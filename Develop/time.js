824e0d57c2a160af960c940009b92d7c91a2622c
maris <marisandb@yahoo.com>
2021-07-03 14:44:22 -0500

    added ability to edit tasks

--- Develop/time.js
+++ Develop/time.js
@@ -1,10 +1,23 @@
 let currentDay = document.querySelector("#currentDay");
+const currentHour = moment().format("k");
 
-//Sets Current Date 
-let setCurrent = function() {
-    
-    let time = moment().format('dddd, MMMM Do, YYYY');
-    currentDay.textContent = time;
-}
-setCurrent();
+let tasksArr = []
 
+//displays date at top of page
+let now = moment().format("dddd, MMM Do, YYYY");
+currentDay.textContent = now;
+
+// allows user to edit task content
+$(document).on("click", ".textarea", function() {
+    // get current text of td element
+    var text = $(this)
+      .text()
+      .trim();
+  
+    // replace td element with a new textarea
+    var textInput = $("<textarea>").val(text);
+    $(this).replaceWith(textInput);
+  
+    // auto focus new element
+    textInput.trigger("focus");
+  });
