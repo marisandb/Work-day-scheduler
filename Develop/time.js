9a012d88ca495f1326de4d1ac52302255ebe4085
maris <marisandb@yahoo.com>
2021-07-04 20:33:23 -0500

    added setColor function

--- Develop/time.js
+++ Develop/time.js
@@ -21,3 +21,23 @@ $(document).on("click", ".textarea", function() {
     // auto focus new element
     textInput.trigger("focus");
   });
+
+  // set function that sets color for past/present/future
+function setColor() {
+    let hourOfDay = $(".time-block")
+    for (let i = 1; i < 10; i++) {
+        let hourOfDayId = document.getElementsByClassName("time-block")[i].id
+        let hourOfDayIdInt = parseInt(hourOfDayId);
+        let currentHourInt = parseInt(currentHour)
+        if (hourOfDay) {
+            if (currentHourInt > hourOfDayIdInt) {
+                hourOfDay[i].setAttribute('class', 'time-block past')
+            } else if (currentHourInt < hourOfDayIdInt) {
+                hourOfDay[i].setAttribute('class', 'time-block future')
+            }
+        }
+
+    }
+}
+
+setColor();
